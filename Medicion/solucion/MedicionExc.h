93113669f96236bbff97b9dcf27dbb8f720775b4
EzeSandes <77078541+EzeSandes@users.noreply.github.com>
2023-05-23 10:52:12 -0300

    Add files via upload

---
+++ Medicion/solucion/MedicionExc.h
@@ -0,0 +1,17 @@
+#ifndef MEDICIONEXC_MEDICIONEXC
+#define MEDICIONEXC_MEDICIONEXC
+
+#include <iostream>
+
+using namespace std;
+
+class MedicionExc
+{
+    private:        string mensaje;
+
+    public:
+        MedicionExc(string mensaje = "ERROR Medicion") : mensaje(mensaje){};
+
+};
+
+#endif // MEDICIONEXC_MEDICIONEXC
